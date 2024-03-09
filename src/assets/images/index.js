3882d80fec63cc17169a9b403187c7d67bda324c
Manohar Singh <manoharsingh@Manohars-MacBook-Pro.local>
2024-03-09 21:03:57 +0530

    Changes done

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,15 +1,25 @@
-import hero from './hero.jpg'
-import meta from './meta.png'
-import shopify from './shopify.png'
-import starbucks from './starbucks.png'
-import tesla from './tesla.png'
-import logo from './logo.svg'
+import hero from "./hero.jpg";
+import meta from "./meta.png";
+import shopify from "./shopify.png";
+import starbucks from "./starbucks.png";
+import tesla from "./tesla.png";
+import logo from "./logo.svg";
+import community from "./community.jpeg";
+import tcs from "./tcs.png";
+import quest from "./quest.png";
+import william from "./william.jpg";
+import happlab from "./happlab.png";
 
 export {
-    hero,
-    meta,
-    shopify,
-    starbucks,
-    tesla,
-    logo
-}
+  hero,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  logo,
+  community,
+  tcs,
+  quest,
+  william,
+  happlab,
+};
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,228 +1,275 @@
-import { meta, shopify, starbucks, tesla } from "../assets/images";
 import {
-    car,
-    contact,
-    css,
-    estate,
-    express,
-    git,
-    github,
-    html,
-    javascript,
-    linkedin,
-    mongodb,
-    motion,
-    mui,
-    nextjs,
-    nodejs,
-    pricewise,
-    react,
-    redux,
-    sass,
-    snapgram,
-    summiz,
-    tailwindcss,
-    threads,
-    typescript
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  community,
+  tcs,
+  quest,
+  william,
+  happlab,
+} from "../assets/images";
+import {
+  car,
+  contact,
+  css,
+  estate,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  linkedin,
+  mongodb,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  pricewise,
+  react,
+  redux,
+  sass,
+  snapgram,
+  summiz,
+  tailwindcss,
+  threads,
+  typescript,
 } from "../assets/icons";
 
 export const skills = [
-    {
-        imageUrl: css,
-        name: "CSS",
-        type: "Frontend",
-    },
-    {
-        imageUrl: express,
-        name: "Express",
-        type: "Backend",
-    },
-    {
-        imageUrl: git,
-        name: "Git",
-        type: "Version Control",
-    },
-    {
-        imageUrl: github,
-        name: "GitHub",
-        type: "Version Control",
-    },
-    {
-        imageUrl: html,
-        name: "HTML",
-        type: "Frontend",
-    },
-    {
-        imageUrl: javascript,
-        name: "JavaScript",
-        type: "Frontend",
-    },
-    {
-        imageUrl: mongodb,
-        name: "MongoDB",
-        type: "Database",
-    },
-    {
-        imageUrl: motion,
-        name: "Motion",
-        type: "Animation",
-    },
-    {
-        imageUrl: mui,
-        name: "Material-UI",
-        type: "Frontend",
-    },
-    {
-        imageUrl: nextjs,
-        name: "Next.js",
-        type: "Frontend",
-    },
-    {
-        imageUrl: nodejs,
-        name: "Node.js",
-        type: "Backend",
-    },
-    {
-        imageUrl: react,
-        name: "React",
-        type: "Frontend",
-    },
-    {
-        imageUrl: redux,
-        name: "Redux",
-        type: "State Management",
-    },
-    {
-        imageUrl: sass,
-        name: "Sass",
-        type: "Frontend",
-    },
-    {
-        imageUrl: tailwindcss,
-        name: "Tailwind CSS",
-        type: "Frontend",
-    },
-    {
-        imageUrl: typescript,
-        name: "TypeScript",
-        type: "Frontend",
-    }
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: express,
+    name: "Express",
+    type: "Backend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  {
+    imageUrl: motion,
+    name: "Motion",
+    type: "Animation",
+  },
+  {
+    imageUrl: mui,
+    name: "Material-UI",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: redux,
+    name: "Redux",
+    type: "State Management",
+  },
+  {
+    imageUrl: sass,
+    name: "Sass",
+    type: "Frontend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
 ];
 
 export const experiences = [
-    {
-        title: "React.js Developer",
-        company_name: "Starbucks",
-        icon: starbucks,
-        iconBg: "#accbe1",
-        date: "March 2020 - April 2021",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-    {
-        title: "React Native Developer",
-        company_name: "Tesla",
-        icon: tesla,
-        iconBg: "#fbc3bc",
-        date: "Jan 2021 - Feb 2022",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-    {
-        title: "Web Developer",
-        company_name: "Shopify",
-        icon: shopify,
-        iconBg: "#b7e4c7",
-        date: "Jan 2022 - Jan 2023",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-    {
-        title: "Full stack Developer",
-        company_name: "Meta",
-        icon: meta,
-        iconBg: "#a2d2ff",
-        date: "Jan 2023 - Present",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
+  {
+    title: "Senior Frontend Developer",
+    company_name: "Community Brands",
+    icon: community,
+    iconBg: "#accbe1",
+    date: "July 2023 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Senior Frontend Developer",
+    company_name: "TCS",
+    icon: tcs,
+    iconBg: "#fbc3bc",
+    date: "May 2021 - July 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Software Engineer",
+    company_name: "Quest Global",
+    icon: quest,
+    iconBg: "#b7e4c7",
+    date: "Nov 2019 - May 2021",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Software Engineer",
+    company_name: "William O'neil India",
+    icon: william,
+    iconBg: "#a2d2ff",
+    date: "Aug 2028 - Nov 2019",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+
+  {
+    title: "Web Developer",
+    company_name: "Happlabs Software llp",
+    icon: happlab,
+    iconBg: "#a2d2ff",
+    date: "Aug 2028 - Nov 2019",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
 ];
 
 export const socialLinks = [
-    {
-        name: 'Contact',
-        iconUrl: contact,
-        link: '/contact',
-    },
-    {
-        name: 'GitHub',
-        iconUrl: github,
-        link: 'https://github.com/YourGitHubUsername',
-    },
-    {
-        name: 'LinkedIn',
-        iconUrl: linkedin,
-        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
-    }
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/manoharkumarsingh/",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/manoharkumarsinghprofile/",
+  },
 ];
 
 export const projects = [
-    {
-        iconUrl: pricewise,
-        theme: 'btn-back-red',
-        name: 'Amazon Price Tracker',
-        description: 'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.',
-        link: 'https://github.com/adrianhajdin/pricewise',
-    },
-    {
-        iconUrl: threads,
-        theme: 'btn-back-green',
-        name: 'Full Stack Threads Clone',
-        description: 'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
-        link: 'https://github.com/adrianhajdin/threads',
-    },
-    {
-        iconUrl: car,
-        theme: 'btn-back-blue',
-        name: 'Car Finding App',
-        description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
-        link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
-    },
-    {
-        iconUrl: snapgram,
-        theme: 'btn-back-pink',
-        name: 'Full Stack Instagram Clone',
-        description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
-        link: 'https://github.com/adrianhajdin/social_media_app',
-    },
-    {
-        iconUrl: estate,
-        theme: 'btn-back-black',
-        name: 'Real-Estate Application',
-        description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
-        link: 'https://github.com/adrianhajdin/projects_realestate',
-    },
-    {
-        iconUrl: summiz,
-        theme: 'btn-back-yellow',
-        name: 'AI Summarizer Application',
-        description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
-        link: 'https://github.com/adrianhajdin/project_ai_summarizer',
-    }
+  {
+    iconUrl: car,
+    theme: "btn-back-blue",
+    name: "Car Finding App",
+    description:
+      "Designed and built a Web app using Nextjs, Tailwind Css, HTML for finding and comparing cars on the market, streamlining the car-buying process.",
+    link: "https://carhub-ten-chi.vercel.app/",
+  },
+
+  {
+    iconUrl: summiz,
+    theme: "btn-back-red",
+    name: "Blog",
+    description:
+      "Crafted an engaging blog website using Next.js, where users seamlessly register, log in, and create captivating content. Incorporated JWT for secure authentication and Tailwind CSS for a sleek design. Implemented a feature-rich user profile, allowing interaction with other users' content, fostering a dynamic and user-friendly experience.",
+    link: "https://carhub-ten-chi.vercel.app/",
+  },
+
+  {
+    iconUrl: summiz,
+    theme: "btn-back-red",
+    name: "Consumer Offers",
+    description:
+      "Created a web application using HTML, advanced React JS, Redux, Thunk, CSS, and JavaScript. Focused on the consumer perspective, the app compares options for buyers by evaluating various features of Apple devices against competitors. Multiple screens with different variations facilitate efficient data comparison.",
+  },
+  {
+    iconUrl: summiz,
+    theme: "btn-back-green",
+    name: "Apple Customer Portal Support",
+    description:
+      "Built a web app using HTML, advanced React JS, Redux, Thunk, CSS, and JavaScript. The app displays advisor performance handled by them, including customer feedback, manager comments, and root cause analysis. Features multiple views like Survey, Peerfeedback, Northstar, Advisor Dashboard, and Metric Viewer for comprehensive insights into advisor performance.",
+  },
+
+  {
+    iconUrl: summiz,
+    theme: "btn-back-pink",
+    name: "William O'Neil India",
+    description:
+      "Created a  web app utilizing HTML, advanced React JS, Vue.js, Vuex, Redux, and Bootstrap CSS. This visually intuitive platform consolidates vital stock information, enabling informed investment decisions. It facilitates efficient stock analysis, watchlist monitoring, and portfolio management for enhanced productivity and smarter investing.",
+    // link: "https://github.com/adrianhajdin/social_media_app",
+  },
+  {
+    iconUrl: summiz,
+    theme: "btn-back-black",
+    name: "Dai Intranet",
+    description:
+      "Developed Dai Intranet, a PHP-based web application with MySQL, HTML, advanced React JS, Redux, Thunk, and Bootstrap CSS. This platform fosters improved communication and collaboration among divisions, aiming to enhance understanding and promote a culture of effectiveness. The site empowers associates, fostering a collaborative environment to better serve customers..",
+    // link: "https://github.com/adrianhajdin/projects_realestate",
+  },
+  {
+    iconUrl: summiz,
+    theme: "btn-back-yellow",
+    name: "Global Policy System",
+    description:
+      "Developed the Global Policy System, a PHP-based web application with PostgreSQL, HTML, advanced React JS, Redux, and Bootstrap CSS. This system serves as a centralized platform for insurance brokers and masters to store and manage data related to insurance holder claims on insured properties.",
+    //link: "https://github.com/adrianhajdin/project_ai_summarizer",
+  },
+  {
+    iconUrl: summiz,
+    theme: "btn-back-yellow",
+    name: "SurgeSend",
+    description:
+      "Developed Surgesend, a PHP-based web application utilizing PostgreSQL, HTML, React JS, Redux, advanced JavaScript, and Bootstrap CSS. This platform simplifies file sharing through links, ensuring all uploads and downloads are tracked via client IP addresses. Users receive a unique access pin generated once, providing secure access to the shared files.",
+    //link: "https://github.com/adrianhajdin/project_ai_summarizer",
+  },
 ];
